d39599fb453cc378cd87ed29ab22325b08b63d31
Anthony Gargano <anthonyg92297@gmail.com>
2019-01-30 02:38:58 -0500

    Update look & feel of playlist page; Add search functionality

--- js/playlists.js
+++ js/playlists.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
     $('body > div').css('height', window.innerHeight + 'px');
+
+    // Initialize Vue and our data
+    var app = new Vue({
+        el: '#app',
+        data: {
+            playlists: [],
+            search: ""
+        },
+        computed: {
+          sortedPlaylists() {
+              var searchList = [];
+              for(playlist in this.playlists) {
+                  if(this.playlists[playlist].name.toLowerCase().includes(this.search)) searchList.push(this.playlists[playlist]);
+              }
+              return _.orderBy(searchList, ['distance'],['asc']);
+          }
+        }
+    });
+    window.vue = app;
     
     var config = {
         apiKey: "AIzaSyBoYhaLK3TtsuN9v0ZxwBS6ozo4PeDbbKU",
@@ -18,36 +37,40 @@ $(document).ready(function() {
 
 });
 
+/**
+ * Gets all the playlists within range of the user
+ */
 async function getPlaylists() {
     var position = await getCurrentLocation();
 
     firebase.database().ref('/Locations').once('value').then(function(locations) {
         locations = locations.val();
-        for(var location in locations) {
-            var playlistIsNear = locationIsNear(locations[location].l, position.coords);
-            if(playlistIsNear) {
-                var playlist = document.createElement('div');
-                playlist.classList.add('playlist');
-                playlist.innerHTML = location;
-                document.getElementById('playlists').appendChild(playlist);
+        for(var playlist in locations) {
+            var dist = getDistance(locations[playlist].l, position.coords);
+            if(dist <= 10) {
+                var p = {
+                    name: playlist,
+                    distance: dist
+                };
+                vue.playlists.push(p);
             }
         }
+
         var status = document.getElementById('status-text');
         status.innerText = 'Select a playlist to join...';
         status.classList.remove('loading');
-        
-        $(".playlist").click(function() {
-            window.location = 'app.html?playlist=' + $(this).html()
-        });
     });
 }
 
-function locationIsNear(checkLoc, currentLoc) {
-    var lng = currentLoc.longitude, lat = currentLoc.latitude, maxDistance = 10; // Distance in km
+/**
+ * Returns the distance in km between two points
+ * @param {coords} checkLoc
+ * @param {coords} currentLoc
+ */
+function getDistance(checkLoc, currentLoc) {
+    var lng = currentLoc.longitude, lat = currentLoc.latitude; // Distance in km
 
-    if ("geolocation" in navigator) {
-
-    } else {
+    if (!("geolocation" in navigator)) {
         // If browser doesn't support geolocation, just add all playlists
         return true;
     }
@@ -57,9 +80,12 @@ function locationIsNear(checkLoc, currentLoc) {
     // Latitude is first in the array and longitude is second
     var dy = Math.abs(lat - checkLoc[0]) * ky; 
     var dx = Math.abs(lng - checkLoc[1]) * kx;
-    return Math.sqrt(dx * dx + dy * dy) <= maxDistance;
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
+/**
+ * Get current location of user
+ */
 function getCurrentLocation() {
     return new Promise(function(resolve, reject) {
         navigator.geolocation.getCurrentPosition(resolve, reject);
